f3ea36ac05172ee721014f55846940bde60b4188
Mrlion <mrlion4202@gmail.com>
2024-01-04 18:16:54 +0000

    some changes in console.log

--- src/events/ready/consoleLog.js
+++ src/events/ready/consoleLog.js
@@ -1,4 +1,7 @@
+const { version } = require('../../../package.json');
+
 module.exports = (client) => {
+  console.log(`🦁 LionBot - ${version}`)
   console.log("🦁 LionBot is starting...")
   console.log(`🦁 LionBot is online.`);
 };
